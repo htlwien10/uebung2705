0064f39ea4f6deb179a34f56395a7c4b25ce307a
Franz <Franz@FK-Desktop.local>
2015-05-26 08:26:53 +0200

    Erweiterung 2

--- Beispiel1/src/Bsp_1.java
+++ Beispiel1/src/Bsp_1.java
@@ -3,7 +3,15 @@ public class Bsp_1 {
 
 	public static void main(String[] args) {
 
+		double seite1, seite2, seite3;
+		
 		titel("Dreieck nach Heron");
+		
+		seite1=IO.readDouble("1. Seitenlänge: ");
+		seite2=IO.readDouble("2. Seitenlänge: ");
+		seite3=IO.readDouble("3. Seitenlänge: ");
+		
+		IO.print("\nDie Fläche beträgt: ");
 		
 		verabschiedung();
 
