44ad326c7eb000eb31571df6b9cae66f24201298
Franz <Franz@FK-Desktop.local>
2015-05-26 08:21:58 +0200

    Erweiterung 1

--- Beispiel1/src/Bsp_1.java
+++ Beispiel1/src/Bsp_1.java
@@ -1,9 +1,34 @@
-
+import AlgoTools.IO;
 public class Bsp_1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt");
+
+		titel("Dreieck nach Heron");
+		
+		verabschiedung();
 
 	}
+	
+	
+	static void titel(String text) {
+		
+		IO.print("\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			IO.print("*");
+		
+		IO.print("\n\t\t*  "+ text + "  *\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			IO.print("*");
+		
+		IO.println("\n");
+	}
+
+	
+	static void verabschiedung() {
+		
+		IO.println("\n\n\t Have A Nice Day!");
+	}	
 
 }
